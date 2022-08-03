97d1c35a3c3faea3c2baddf84c983de7dff9a112
yakubalkis <yakupalkiis@gmail.com>
2022-08-03 21:43:12 +0300

    Source scripts completed.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../src/components/header'
+import Meme from '../src/components/meme'
+
+
+export default function App(){
+    return (
+        <div className="container">
+            <Header/>
+            <Meme/>
+        </div>
+    )
+}
